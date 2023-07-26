eec2ea69a833655e9b726d455057a66dd0c047a9
Breeze <big_fw@foxmail.com>
2023-07-26 21:38:07 +0800

    coding life

---
+++ 2. gcc/1. gcc命令/hello.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+        > File Name: hello.c
+        > Author: echo
+        > Mail: echobydq@gmail.com
+        > Created Time: Wed 26 Jul 2023 09:28:22 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+
+    return 0;
+}
---
+++ 2. gcc/2. 静态库和动态库/dynamicLib/inc/mymath.h
@@ -0,0 +1,16 @@
+/*************************************************************************
+        > File Name: mymath.h
+        > Author: echo
+        > Mail: echobydq@gmail.com
+        > Created Time: Wed 26 Jul 2023 05:49:43 PM CST
+ ************************************************************************/
+
+#ifndef _MYMATH_H
+#define _MYMATH_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div1(int a, int b);
+
+#endif
